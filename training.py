8ea20d3ee64cf458b540f31b3e8a31a8277afae8
ahmedsayedtalib <ahmedsayed747@hotmail.com>
2025-01-16 18:40:01 +0200

    Manipulating, Cleaning, Inspecting

---
+++ training.py
@@ -0,0 +1,31 @@
+from datasets import load_dataset
+import pandas as pd
+import numpy as np
+
+
+dataset = load_dataset('lukebarousse/data_jobs')
+dataframe = dataset['train'].to_pandas()
+dataframe.posting_date.dt.days_in_month
+
+print(dataframe.info())
+print(dataframe[(dataframe.job_title == 'Data Analyst') & (dataframe.salary_year_avg > 100000)])
+print(dataframe.job_title_short.unique())
+dataAnalyst = dataframe[['job_title','salary_year_avg']][(dataframe.job_title_short == 'Data Analyst') & (dataframe.salary_year_avg > 100000) & (dataframe.salary_year_avg != 'NaN')]
+
+dataAnalystAvgSalary = np.mean(dataAnalyst.salary_year_avg)
+dataAnalystMinSalary = np.min(dataAnalyst.salary_year_avg)
+dataAnalystMaxSalary = np.max(dataAnalyst.salary_year_avg)
+print(dataAnalystAvgSalary.round(1))
+print(dataAnalystMaxSalary)
+print(dataAnalystMinSalary)
+print(dataAnalyst.count())
+
+print(pd.to_datetime(dataframe.job_posted_date))
+print(dataframe[['job_title_short','salary_year_avg']].sort_values(by='salary_year_avg',ascending=True)
+[(dataframe.job_title_short == 'Data Analyst')&(dataframe.salary_year_avg > 100_000)&(dataframe.salary_year_avg != 'NaN')])
+
+print(dataframe.drop(labels='job_title_short',axis=1))
+print(dataframe.dropna(subset='salary_year_avg'),axis=1)
+
+
+dataframe[['job_title_short','salary_year_avg','job_country']].sort_values('salary_year_avg')[(dataframe.salary_year_avg > 100000)&(dataframe.job_country == 'Sudan')]
